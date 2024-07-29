b75281ab90cd0e8f5b44b025d17ebe26d57f8029
Koshila Sandaru <koshila@bistecglobal.com>
2024-07-29 21:05:45 +0530

    Tried Custom Transformers

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,23 +1,10 @@
-/*
- * Hi!
- *
- * Note that this is an EXAMPLE Backstage backend. Please check the README.
- *
- * Happy hacking!
- */
-
 import { createBackend } from '@backstage/backend-defaults';
-//Using custom Transformers
 import { createBackendModule } from '@backstage/backend-plugin-api';
 import { microsoftGraphOrgEntityProviderTransformExtensionPoint } from '@backstage/plugin-catalog-backend-module-msgraph/alpha';
-import {
-  myUserTransformer,
-} from './transformers';
+import { myUserTransformer } from './transformers';
 
-
-// Load environment variables from the .env file
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') })
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 const backend = createBackend();
 
@@ -26,7 +13,6 @@ backend.add(import('@backstage/plugin-proxy-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
-//Custom transformer
 backend.add(
   createBackendModule({
     pluginId: 'catalog',
@@ -34,46 +20,28 @@ backend.add(
     register(env) {
       env.registerInit({
         deps: {
-          microsoftGraphTransformers:
-            microsoftGraphOrgEntityProviderTransformExtensionPoint,
+          microsoftGraphTransformers: microsoftGraphOrgEntityProviderTransformExtensionPoint,
         },
         async init({ microsoftGraphTransformers }) {
           microsoftGraphTransformers.setUserTransformer(myUserTransformer);
         },
       });
     },
-  }),
+  })
 );
 
-
-// auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
-// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-
-//Auth plgin added for the microsoft provider
 backend.add(import('@backstage/plugin-auth-backend-module-microsoft-provider'));
-// See https://backstage.io/docs/auth/guest/provider
+backend.add(import('@backstage/plugin-auth-backend-module-google-provider'));
 
-// catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
-backend.add(
-  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
-);
-
-// permission plugin
+backend.add(import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'));
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
-backend.add(
-  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-);
-
-// search plugin
+backend.add(import('@backstage/plugin-permission-backend-module-allow-all-policy'));
 backend.add(import('@backstage/plugin-search-backend/alpha'));
-//Added msgraph alpha
 backend.add(import('@backstage/plugin-catalog-backend-module-msgraph/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
-
-
 
 backend.start();
